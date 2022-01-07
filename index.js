3eafc77a4ec5658d7ada2096d31e3335cc8945f2
Bryce-Robinson <berobinson83@gmail.com>
2022-01-07 10:48:50 -0500

    Final Code

--- index.js
+++ index.js
@@ -1,56 +1,89 @@
-const PORT = 8000
+const PORT = process.env.PORT || 8001
 const express = require('express')
 const axios = require('axios').default;
 const cheerio = require('cheerio')
-
 const app = express()
-
-const newspapers = [
-    {
+// ------------ Add WebSites to Scrape for API -----------
+const newspapers = [{
         name: 'cnbc',
         address: 'https://www.cnbc.com/marijuana/',
+        base: ''
     },
     {
-        name:'apNews',
-        address: 'https://apnews.com/hub/marijuana'
+        name: 'apNews',
+        address: 'https://apnews.com/hub/marijuana',
+        base: 'https://apnews.com/hub/marijuana/'
     },
     {
-        name:'highTimes',
-        address: 'https://hightimes.com/news/'
+        name: 'highTimes',
+        address: 'https://hightimes.com/news/',
+        base: ''
     }
 ]
+
+
 const articles = []
 
-// '/' listens to homepage
-
-
-
-newspapers.forEach(newspaper =>{
+newspapers.forEach(newspaper => {
     axios.get(newspaper.address)
-    .then(response =>{
-        const html = response.data
-        const $ = cheerio.load(html)
+        .then(response => {
+            const html = response.data
+            const $ = cheerio.load(html)
 
-        $('a:contains("cannabis")', html).each(function () {
-            const title = $(this).text()
-            const url = $(this).attr('href')
+            $('a:contains("cannabis")', html).each(function () {
+                const title = $(this).text()
+                const url = $(this).attr('href')
 
-            articles.push({
-                title,
-                url,
-                source: newspaper.name
+                articles.push({
+                    title,
+                    url: newspaper.base + url,
+                    source: newspaper.name
+                })
             })
-    })
-})})
+        })
+})
 
-app.get('/',(req,res)=>{
-    
+app.get('/', (req, res) => {
+
     res.json('Welcome to my SmokeShop API')
 })
 
-app.get('/News', (req,res)=>{
+app.get('/news', (req, res) => {
     res.json(articles)
 
 })
 
-app.listen(PORT, ()=> console.log(`server running on ${PORT}`))
+
+app.get('/news/:newspaperId', (req, res) => {
+
+
+
+    const newspaperId = req.params.newspaperId
+
+    const newspaperAddress = newspapers.filter(newspaper => newspaper.name == newspaperId)[0].address
+    // https://youtu.be/GK4Pl-GmPHk?t=2779
+    const newspaperBase = newspapers.filter(newspaper => newspaper.name == newspaperId)[0].base
+
+
+    // https://youtu.be/GK4Pl-GmPHk?t=2665
+    axios.get(newspaperAddress).then(response => {
+        const html = response.data
+        const $ = cheerio.load(html)
+        const specificArticles = []
+
+        $('a:contains("cannabis")', html).each(function () {
+            const title = $(this).text()
+            const url = $(this).attr('href')
+            specificArticles.push({
+                title,
+                url: newspaperBase + url,
+                source: newspaperId
+            })
+        })
+        res.json(specificArticles)
+
+    }).catch(err => console.log(err))
+})
+
+
+app.listen(PORT, () => console.log(`server running on "https//localhost:${PORT}/"`))
